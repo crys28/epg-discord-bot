5489684c07e50e1c85fb49eac23ab51cf8a776a8
Crys28 <crys_gaming@yahoo.com>
2022-08-10 14:12:04 +0300

    adding express.js server

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
 const request = require('request-promise');
 const cheerio = require('cheerio');
 const fs = require('fs');
